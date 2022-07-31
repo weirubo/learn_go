4f264a7f144a4063209a94cc14d34dbd32eb7e9d
weirubo <weirubo@gmail.com>
2022-07-31 15:50:01 +0800

    memory escape

---
+++ lesson44/go.mod
@@ -0,0 +1,3 @@
+module github.com/weirubo/learn_go/lesson44
+
+go 1.13
---
+++ lesson44/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	// pointerEscape(1, 2)
+}
+
+// pointerEscape
+// func pointerEscape(a, b int) *int {
+// 	sum := a + b
+// 	return &sum
+// }
